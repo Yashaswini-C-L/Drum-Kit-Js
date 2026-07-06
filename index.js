c5c300acc37dc473fad2bd7e2ef1d59e908b8878
Yashaswini-C-L <170310182+Yashaswini-C-L@users.noreply.github.com>
2026-07-06 22:04:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const sounds = [
+  "./sounds/crash.mp3",
+  "./sounds/kick-bass.mp3",
+  "./sounds/snare.mp3",
+  "./sounds/tom-1.mp3",
+  "./sounds/tom-2.mp3",
+  "./sounds/tom-3.mp3",
+  "./sounds/tom-4.mp3",
+];
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    const audio = new Audio(sounds[i]);
+    audio.play();
+  });
+}
